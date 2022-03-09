99cfd6b2b3023434666a91cbc58e4c181f73f11f
Damian Gurdak <damiangurdakk@gmail.com>
2022-03-09 21:47:11 +0100

    Add function displayResult and getMoveName

--- js/script.js
+++ js/script.js
@@ -1,3 +1,38 @@
+function getMoveName(argMoveId) {
+	if (argMoveId == 1) {
+		return "kamień";
+	} else if (argMoveId == 2) {
+		return "papier";
+	} else if (argMoveId == 3) {
+		return "nożyce";
+	}
+
+	printMessage("Nie znam ruchu o id " + argMoveId + ".");
+	return "nieznany ruch";
+}
+
+function displayResult(argComputerMove, argPlayerMove) {
+	console.log("moves:", argComputerMove, argPlayerMove);
+
+	if (
+		(argComputerMove == "kamień" && argPlayerMove == "papier") ||
+		(argComputerMove == "papier" && argPlayerMove == "nożyce") ||
+		(argComputerMove == "nożyce" && argPlayerMove == "kamień")
+	) {
+		printMessage("Ty wygrywasz!");
+	} else if (
+		(argComputerMove == "papier" && argPlayerMove == "kamień") ||
+		(argComputerMove == "nożyce" && argPlayerMove == "papier") ||
+		(argComputerMove == "kamień" && argPlayerMove == "nożyce")
+	) {
+		printMessage("Wygrał komputer!");
+		/*} else if (playerMove == "nieznany ruch") {
+		printMessage("nie znam takiego ruchu, spróbuj jeszcze raz!");*/
+	} else if (argComputerMove == argPlayerMove) {
+		printMessage("Mamy Remis! Zagraj ponowanie");
+	}
+}
+
 function printMessage(msg) {
 	let div = document.createElement("div");
 	div.innerHTML = msg;
@@ -12,15 +47,15 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log("Wylosowana liczba to: " + randomNumber);
 
-let computerMove = "nieznany ruch";
+let computerMove = getMoveName(randomNumber);
 
-if (randomNumber == 1) {
+/*if (randomNumber == 1) {
 	computerMove = "kamień";
 } else if (randomNumber == 2) {
 	computerMove = "papier";
 } else if (randomNumber == 3) {
 	computerMove = "nożyce";
-}
+}*/
 
 printMessage("Mój ruch to: " + computerMove);
 
@@ -28,19 +63,19 @@ let playerInput = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.
 
 console.log("Gracz wpisał: " + playerInput);
 
-let playerMove = "nieznany ruch";
+let playerMove = getMoveName(playerInput);
 
-if (playerInput == "1") {
+/*if (playerInput == "1") {
 	playerMove = "kamień";
 } else if (playerInput == "2") {
 	playerMove = "papier";
 } else if (playerInput == "3") {
 	playerMove = "nożyce";
-}
+}*/
 
 printMessage("Twój ruch to: " + playerMove);
 
-if (
+/*if (
 	(computerMove == "kamień" && playerMove == "papier") ||
 	(computerMove == "papier" && playerMove == "nożyce") ||
 	(computerMove == "nożyce" && playerMove == "kamień")
@@ -52,8 +87,10 @@ if (
 	(computerMove == "kamień" && playerMove == "nożyce")
 ) {
 	printMessage("Wygrał komputer!");
-} else if (playerMove == "nieznany ruch") {
+	} else if (playerMove == "nieznany ruch") {			//powiela sie kom z getNameMove
 	printMessage("nie znam takiego ruchu, spróbuj jeszcze raz!");
 } else if (computerMove == playerMove) {
 	printMessage("Mamy Remis! Zagraj ponowanie");
-}
+} */
+
+displayResult(computerMove, playerMove);
